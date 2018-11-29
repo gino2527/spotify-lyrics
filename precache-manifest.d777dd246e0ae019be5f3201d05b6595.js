eb558379432a7096416f4476fd10d2f93e628c54
gino2527 <gino.cabullo@obf.ateneo.edu>
2018-11-29 19:24:41 +0800

    Updates

---
+++ precache-manifest.d777dd246e0ae019be5f3201d05b6595.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/spotify-lyrics/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "4324f00ee3eed97f192e",
+    "url": "/spotify-lyrics/static/js/runtime~main.4324f00e.js"
+  },
+  {
+    "revision": "6537fdf8dd7db758d539",
+    "url": "/spotify-lyrics/static/js/main.6537fdf8.chunk.js"
+  },
+  {
+    "revision": "1da3fd2ac8b30cd20692",
+    "url": "/spotify-lyrics/static/js/1.1da3fd2a.chunk.js"
+  },
+  {
+    "revision": "6537fdf8dd7db758d539",
+    "url": "/spotify-lyrics/static/css/main.036c7bc4.chunk.css"
+  },
+  {
+    "revision": "4b6a4424f091c4a53357bc8188e89641",
+    "url": "/spotify-lyrics/index.html"
+  }
+];
