fe3cff054380fe80c2d03d676fa1c600c3731d2f
gino2527 <gino.cabullo@obf.ateneo.edu>
2018-11-30 19:00:27 +0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/spotify-lyrics/precache-manifest.40c7d2a94b004e5de36da803bd28cf6a.js"
+  "/spotify-lyrics/precache-manifest.e1c05cd1212d3f5be54c8fce57077ad6.js"
 );
 
 workbox.clientsClaim();
